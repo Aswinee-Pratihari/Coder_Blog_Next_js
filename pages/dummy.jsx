a5eb65c9ebc88e5c51483eeac5a80f6290cce6c2
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-01-09 18:32:26 +0530

    Styled JSX and global styling

---
+++ pages/dummy.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <>
+    
+    <style jsx global>
+{`.styling{
+
+    background:white;
+    text-align:center;
+}
+`}
+
+    </style>
+    <div>
+      I am a dummy text
+    </div>
+    </>
+  )
+}
+
+export default Dummy
